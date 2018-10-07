3f121c48690ebefee9fdebb93bc70d4b35d30d6d
rubbish-man <993284758@qq.com>
2018-10-07 11:32:48 +0800

    header

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import {
-	HeaderWrapper
+	HeaderWrapper,
+	Logo,
+	Nav,
+	NavItem,
+	NavSearch,
+	Addtion,
+	Button
 } from './style';
 
 
 class Header extends Component {
 	render() {
 		return (
-			<HeaderWrapper>header</HeaderWrapper>
+			<HeaderWrapper>
+				<Logo />
+				<Nav>
+					<NavItem className='left active'>首页</NavItem>
+					<NavItem className='left'>下载App</NavItem>
+					<NavItem className='right'>登录</NavItem>
+					<NavItem className='right'>Aa</NavItem>
+					<NavSearch></NavSearch>
+				</Nav>
+
+				<Addtion>
+					<Button className='reg'>注册</Button>
+					<Button className='writting'>写文章</Button>
+				</Addtion>
+			</HeaderWrapper>
 		)
 	}
 }
