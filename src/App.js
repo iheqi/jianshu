72c795637ec5c4bb6509c5e0eff42a2e4caef7e3
rubbish-man <993284758@qq.com>
2018-12-10 20:01:19 +0800

    路由

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom'; 
 import Header from './common/header';
 import store from './store';
-import { Provider } from 'react-redux';
+
+import Home from './pages/home';
+import Detail from './pages/detail';
 
 class App extends Component {
   render() {
     return (
     	<Provider store={store}>
+        <div>
         <Header />
+        <BrowserRouter>
+          <div>
+            <Route exact path='/' component={Home}></Route>
+            <Route exact path='/detail' component={Detail}></Route>
+          </div>
+        </BrowserRouter>          
+        </div>
       </Provider>
     );
   }
---
+++ src/pages/home/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { HomeWrapper, HomeLeft, HomeRight } from './style';
+import List from './components/List';
+import Topic from './components/Topic';
+import Writer from './components/Writer';
+import Recommend from './components/Recommend';
+class Home extends Component {
+  render() {
+    return (
+      <HomeWrapper>
+        <HomeLeft>
+          <img className="banner-img" src="//upload.jianshu.io/admin_banners/web_images/4581/313b30a87782c9934e69fa46cfd7df0549e55aea.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"></img>
+          <List/>
+          <Topic/>
+        </HomeLeft>
+        <HomeRight>
+          <Writer/>
+          <Recommend/>
+        </HomeRight>
+      </HomeWrapper>
+    )
+  }
+}
+
+export default Home;
